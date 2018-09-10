e7ce9f18ace6fd527edacc13ab29f75794cf22aa
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-10 03:43:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-def GET/
-  puts "Hello, World!"
+  def GET/
+    puts "Hello, World!"
 
-end
+  end
 
 end
